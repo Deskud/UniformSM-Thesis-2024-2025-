45186f131be3573443b619e59bc1f95049a5e66c
Deskud <darrylgambe@gmail.com>
2024-06-09 14:41:42 +0800

    Fixed responsiveness- deleted purchase and product page for the meantime -

--- Dashboard.php
+++ Dashboard.php
@@ -1,115 +1,151 @@
 <?php
-  session_start();
+session_start();
 
 
-  //Basically checks if the session is the same credentials entered, if its incorrect it will redirect the user to the Login page. 
-  $username = ($_SESSION['admin']);
-  if(!isset($_SESSION['admin'])) header('location: Login.php');
-  $username = $_SESSION['admin'];
+//Basically checks if the session is the same credentials entered, if its incorrect it will redirect the user to the Login page. 
+$username = ($_SESSION['admin']);
+if (!isset($_SESSION['admin'])) header('location: Login.php');
+$username = $_SESSION['admin'];
 
 
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <script src="https://kit.fontawesome.com/883b8ee9d9.js" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="Styles/Stylings.css">
-    <title>Dashboard</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  <script src="https://kit.fontawesome.com/883b8ee9d9.js" crossorigin="anonymous"></script>
+  <link rel="stylesheet" href="Styles/Stylings.css">
+  <title>Dashboard</title>
 </head>
-<body class="Dashboard-main">
 
-
-<!-- Navigation bar -->
-
-<?php include "Parts/NavigationBar.php";?>
-
-<!-- End Navbar -->
-
-
-
-<!-- Sidebar -->
-
-<div id="Sidebar-bootstrap" class="shadow-sm d-flex flex-column flex-shrink-0 p-3 bg-body-tertiary bg-dark" style="width: 280px;">
+<body class="Pages-main-container">
+  <!-- Navigation bar -->
+  <?php include "Parts/NavigationBar.php"; ?>
+  <!-- Sidebar -->
+  <div id="Sidebar-bootstrap" class="shadow-sm d-flex flex-column flex-shrink-0 p-3 text-bg-dark side d-none d-xl-block col" style="width: 270px;">
     <a href="#" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
-        <span class="fs-4">
-        <?= $_SESSION['admin'];?>
-        </span>
+      <span class="fs-4">
+        <?= $_SESSION['admin']; ?>
+      </span>
     </a>
-<hr>
-<ul class="nav nav-pills flex-column mb-auto">
+    <hr>
+    <ul class="nav nav-pills flex-column mb-auto">
 
-        <a href="#" class="nav-link active" aria-current="page">
+      <a href="#" class="nav-link active" aria-current="page">
         <i class="fa-solid fa-gauge"></i>
-          Dashboard
-        </a>
+        Dashboard
+      </a>
       </li>
       <li>
-        <a href="Purchase.php" class="nav-link text-white">
-        <i class="fa-solid fa-arrow-trend-up"></i>
+        <a href="#" class="nav-link text-white">
+          <i class="fa-solid fa-arrow-trend-up"></i>
           Purchase History
         </a>
       </li>
       <li>
-        <a href="Products.php" class="nav-link text-white">
-        <i class="fa-solid fa-shirt"></i>
+        <a href="#" class="nav-link text-white">
+          <i class="fa-solid fa-shirt"></i>
           Products
         </a>
       </li>
       <li>
         <a href="#" class="nav-link text-white">
-        <i class="fa-solid fa-qrcode"></i>
+          <i class="fa-solid fa-qrcode"></i>
           QR Code
         </a>
+      </li>
+      <hr>
+      <div class="sidebar-footer">
+        <a class="sidebar-logout" href="Logout.php">
+          <i class="fa-solid fa-right-from-bracket"></i>
+          <span>Sign out</span>
+        </a>
+      </div>
     </ul>
   </div>
+  <div class="Main-content">
+    <div class="dashboard-content">
+      <!-- Dashboard Table  -->
+      <div class="container-xl">
+        <div class="table-responsive">
+          <table class="table table-bordered col-xl">
+            <thead>
+              <tr>
+                <th scope="col">Product</th>
+                <th scope="col">Size</th>
+                <th scope="col">Price</th>
+                <th scope="col">Quantity</th>
+              </tr>
+            </thead>
 
-<!-- End Sidebar -->
+            <tbody>
+              <td>PE Uniform </td>
+              <td>Large </td>
+              <td>P350.00 </td>
+              <td>20 </td>
+            </tbody>
+            <tbody>
+              <td>PE Uniform </td>
+              <td>Large </td>
+              <td>P350.00 </td>
+              <td>20 </td>
+            </tbody>
+            <tbody>
+              <td>PE Uniform </td>
+              <td>Large </td>
+              <td>P350.00 </td>
+              <td>20 </td>
+            </tbody>
+            <tbody>
+              <td>PE Uniform </td>
+              <td>Large </td>
+              <td>P350.00 </td>
+              <td>20 </td>
+            </tbody>
+            <tbody>
+              <td>PE Uniform </td>
+              <td>Large </td>
+              <td>P350.00 </td>
+              <td>20 </td>
+            </tbody>
+            <tbody>
+              <td>PE Uniform </td>
+              <td>Large </td>
+              <td>P350.00 </td>
+              <td>20 </td>
+            </tbody>
+            <tbody>
+              <td>PE Uniform </td>
+              <td>Large </td>
+              <td>P350.00 </td>
+              <td>20 </td>
+            </tbody>
+          </table>
+        </div>
+      </div>
+      <!-- Page Search -->
+      <nav aria-label="Search results pages">
+        <ul class="pagination justify-content-center">
+          <li class="page-item disabled">
+            <a class="page-link">Previous</a>
+          </li>
+          <li class="page-item"><a class="page-link" href="#">1</a></li>
+          <li class="page-item"><a class="page-link" href="#">2</a></li>
+          <li class="page-item"><a class="page-link" href="#">3</a></li>
+          <li class="page-item">
+            <a class="page-link" href="#">Next</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  </div>
 
-
-<!-- Dashboard Table  -->
-
-<table class="table table-bordered" style="width: 50%;">
-  <thead>
-    <tr>
-      <th scope="col">Product</th>
-      <th scope="col">Size</th>
-      <th scope="col">Price</th>
-      <th scope="col">Quantity</th>
-    </tr>
-  </thead>
-    <td>PE Uniform </td>
-    <td>Large </td>
-    <td>P350.00 </td>
-    <td>20 </td>
-  <tbody>
-  
-  </tbody>
-</table>
-
-
-<!-- End Dashboard Table -->
-
-
-<!-- Page Search -->
-<nav aria-label="Search results pages">
-  <ul class="pagination justify-content-center">
-    <li class="page-item disabled">
-      <a class="page-link">Previous</a>
-    </li>
-    <li class="page-item"><a class="page-link" href="#">1</a></li>
-    <li class="page-item"><a class="page-link" href="#">2</a></li>
-    <li class="page-item"><a class="page-link" href="#">3</a></li>
-    <li class="page-item">
-      <a class="page-link" href="#">Next</a>
-    </li>
-  </ul>
-</nav>
-
-<!-- End Page Search -->
 
 </body>
+
 </html>
--- Login.php
+++ Login.php
@@ -1,101 +1,101 @@
 <?php
-    include "dbconnection.php";
+include "dbconnection.php";
 
-    session_start();
-    
+session_start();
 
 
-    if(isset($_POST['submit'])){
-        $username = $_POST['user'];//name of the username input is "user"
-        $password = $_POST['pass'];//name of the password input is "pass"
 
-        //selects db table name (user_admin) where columns (user_name and password) is checked if the same as the user input
-        $sql = "SELECT * from user_admin where user_name = '$username' && password = '$password'"; 
+if (isset($_POST['submit'])) {
+    $username = $_POST['user']; //name of the username input is "user"
+    $password = $_POST['pass']; //name of the password input is "pass"
 
-        $result = mysqli_query($conne, $sql);
-        $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
-        $count = mysqli_num_rows($result);
+    //selects db table name (user_admin) where columns (user_name and password) is checked if the same as the user input
+    $sql = "SELECT * from user_admin where user_name = '$username' && password = '$password'";
 
-        //basically verifies if the connection to the database is correct (right credentials). After that it will redirect user to the Dashboard.php
-        if($count==1){
-           $_SESSION['admin'] = $username;
-            header("Location: Dashboard.php");
-        }
-        else{
+    $result = mysqli_query($conne, $sql);
+    $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+    $count = mysqli_num_rows($result);
 
-            //Show alert message if credentials input does not exist on the database.
-            echo'<script>
+    //basically verifies if the connection to the database is correct (right credentials). After that it will redirect user to the Dashboard.php
+    if ($count == 1) {
+        $_SESSION['admin'] = $username;
+        header("Location: Dashboard.php");
+    } else {
+
+        //Show alert message if credentials input does not exist on the database.
+        echo '<script>
                     alert("These credentials do not match our records.");
                 </script>';
-        }
     }
-   
+}
+
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://kit.fontawesome.com/883b8ee9d9.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="Styles/Stylings.css">
 
-    
+
     <title>Uniform-Stocks</title>
 </head>
+
 <body id="Login-Logout">
-        <img src="Styles/BGpics/PCU Logo.png" class="pculogo">
+    <img src="Styles/BGpics/PCU Logo.png" class="pculogo">
 
-        <div class="LoginContainer">
-            <form name="form" action="Login.php" class="LoginBox" onsubmit="return isvalid()" method="post">
-                    <h3>Login to your account</h3>
-                    
-                    <div class="Login-content">
-                        <i class="fa-solid fa-user"></i>
-                        <input type="text" placeholder="Username" name= "user">
-                    </div>
+    <div class="LoginContainer">
+        <form name="form" action="Login.php" class="LoginBox" onsubmit="return isvalid()" method="post">
+            <h3>Login to your account</h3>
 
-                    <div class="Login-content">
-                        <i class="fa-solid fa-lock"></i>
-                        <input type="password" placeholder="Password" name="pass">
-                    </div>
+            <div class="Login-content">
+                <i class="fa-solid fa-user"></i>
+                <input type="text" placeholder="Username" name="user">
+            </div>
 
-                    <!-- <div class="Login-checkbox">
+            <div class="Login-content">
+                <i class="fa-solid fa-lock"></i>
+                <input type="password" placeholder="Password" name="pass">
+            </div>
+
+            <!-- <div class="Login-checkbox">
                         <input type="checkbox" name="remember">  Remember me
                         
                     </div> -->
 
-                    <div class="Login-Button-content">
-                        <input type="submit" class="Login-Button" value="Login" name="submit">
-                    </div>
-            </form>
-        </div>
-        <script>
+            <div class="Login-Button-content">
+                <input type="submit" class="Login-Button" value="Login" name="submit">
+            </div>
+        </form>
+    </div>
+    <script>
+        //Show alert messages if user and pass is empty or both are empty.
 
-            //Show alert messages if user and pass is empty or both are empty.
 
-
-            function isvalid(){
-                var user = document.form.user.value;
-                var pass = document.form.pass.value;
-                if(user.length=="" && pass.length==""){
-                    alert("Enter Credentials");
-                    return false
-                }
-                else{
-                    if(user.length==""){
+        function isvalid() {
+            var user = document.form.user.value;
+            var pass = document.form.pass.value;
+            if (user.length == "" && pass.length == "") {
+                alert("Enter Credentials");
+                return false
+            } else {
+                if (user.length == "") {
                     alert("Enter Username");
                     return false
                 }
-                if(pass.length==""){
+                if (pass.length == "") {
                     alert("Enter Password");
                     return false
                 }
             }
         }
-        </script>
+    </script>
 
-     
+
 </body>
+
 </html>
